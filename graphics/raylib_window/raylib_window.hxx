55bc329934dd24fc3ad934116e737feba012abea
matt <myounger009@gmail.com>
2022-10-09 15:16:09 +0100

    Changes

--- graphics/raylib_window/raylib_window.hxx
+++ graphics/raylib_window/raylib_window.hxx
@@ -1,10 +1,28 @@
 #include <raylib.h>
-#include "types/type_alias.h"
+#include "types/type_alias.hxx"
 
 class RaylibWindow {
-  RaylibWindow(u32 w, u32 h);
+public:
+  RaylibWindow(const u32 w, const u32 h, const char* title);
+
+  void update();
+
+  RaylibWindow& setTargetFPS(const u32 fps);
+
+  ~RaylibWindow();
 };
 
 #ifdef RAYLIB_WINDOW_IMPL
+RaylibWindow::RaylibWindow(const u32 w, const u32 h, const char* title) {
+    InitWindow(w, h, title);
+}
 
+RaylibWindow& RaylibWindow::setTargetFPS(const u32 fps) {
+  SetTargetFPS(fps);
+  return this;
+}
+
+RaylibWindow::~RaylibWindow() {
+  CloseWindow();
+}
 #endif
